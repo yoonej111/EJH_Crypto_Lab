eb1a2d0bb26bb3ae7e995c7ab5336680dce70dae
eunjaeyoon <yoonej111@gmail.com>
2022-05-30 00:12:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home" className="companyname">
+                        EJH Crypto Labs
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features" className="menu">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href="#pricing" className="menu">
+                                Member
+                            </Nav.Link>
+                            <Nav.Link href="#pricing" className="menu">
+                                Project
+                            </Nav.Link>
+                            <NavDropdown
+                                title="Dropdown"
+                                id="collasible-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/3.1">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Something
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div id="main">메인화면</div>
+            <div id="member">Members</div>
+            <div id="project">Project</div>
+            <footer>Copyright by EJH Crypto labs@2022</footer>
+        </div>
+    );
 }
 
 export default App;
